f61ac27d26c7f0bde15937817e6b014fa5aee87c
ata kaleli <atakaleli123@gmail.com>
2024-07-16 14:52:23 +0300

    Player wallDetection and jump/fall anim is added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,14 +12,23 @@ public class Player : MonoBehaviour
     [Header("Player Movements")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    private int facingDirection;
+    private bool isFacingRight;
+    private bool canDoubleJump;
 
     [Header("Collision Checks - Ground")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
-    private bool canDoubleJump;
-    
+
+    [Header("Collision Checks - Wall")]
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsWall;
+    private bool isWallDetected;
+
+
 
 
     void Start()
@@ -27,6 +36,8 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         canDoubleJump = true;
+        facingDirection = 1;
+        isFacingRight = true;
     }
 
     
@@ -34,13 +45,13 @@ public class Player : MonoBehaviour
     {
         Move();
         CollisionChecks();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             JumpController();
         }
 
-
-
+        FlipController();
         AnimationController();
     }
 
@@ -82,10 +93,29 @@ public class Player : MonoBehaviour
     private void CollisionChecks()
     {
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance,whatIsGround);
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right * facingDirection, wallCheckDistance, whatIsWall);
     }
+
+    private void Flip()
+    {
+        transform.Rotate(new Vector3(0, 180, 0));
+        isFacingRight = !isFacingRight;
+        facingDirection *= -1;
+    }
+
+    private void FlipController()
+    {
+        if (isFacingRight && rb.velocity.x < 0)
+            Flip();
+        else if (!isFacingRight && rb.velocity.x > 0)
+            Flip();
+    }
+
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance, groundCheck.position.z));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance * facingDirection, wallCheck.position.y, wallCheck.position.z));
+        
     }
 }
