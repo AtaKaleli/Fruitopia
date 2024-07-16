cb6fc47c69fdcac6a861582610c9e3686f84f7b8
ata kaleli <atakaleli123@gmail.com>
2024-07-16 13:22:41 +0300

    Player basicMovement and Jump is added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+
+    [Header("Player Movements")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Checks - Ground")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool canDoubleJump;
+    
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        canDoubleJump = true;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Move();
+        CollisionChecks();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpController();
+        }
+
+    }
+
+    private void JumpController()
+    {
+        if (isGrounded)
+        {
+            Jump(1);
+            canDoubleJump = true;
+        }
+        else
+        {
+            if (canDoubleJump)
+            {
+                Jump(0.75f);
+                canDoubleJump = false;
+            }
+        }
+    }
+
+    private void Jump(float jumpForceMultiplier)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce*jumpForceMultiplier);
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveSpeed * horizontalInput, rb.velocity.y);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance,whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance, groundCheck.position.z));
     }
 }
