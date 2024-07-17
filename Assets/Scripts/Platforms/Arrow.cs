27fd4ac20b7280c4581a7e274582279e86ef52e7
ata kaleli <atakaleli123@gmail.com>
2024-07-17 14:08:08 +0300

    Platform_Arrow is added

---
+++ Assets/Scripts/Platforms/Arrow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private Vector2 arrowForce;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+            playerRb.velocity = arrowForce;
+            anim.SetTrigger("gotHit");
+        }
+    }
+
+    private void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
