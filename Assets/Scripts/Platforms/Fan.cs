6f6e8b85c79f612c264747ea33248704956395e2
ata kaleli <atakaleli123@gmail.com>
2024-07-19 23:01:18 +0300

    Platform_Fan is added
    Fan is working well, but later on, it needs particle effect, something looks like a wind

---
+++ Assets/Scripts/Platforms/Fan.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    private BoxCollider2D bc;
+    private Animator anim;
+
+    [SerializeField] private float idleTime;
+    [SerializeField] private float activeTime;
+    [SerializeField] private float pushUpwardForce;
+    [SerializeField] private Vector2 bcSize;
+    private float idleTimeCounter;
+    private bool isWorking;
+
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        idleTimeCounter = idleTime;
+
+        bc.size = bcSize;
+        bc.offset = new Vector2(0f, (bcSize.y) / 2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        idleTimeCounter -= Time.deltaTime;
+        
+
+        if (idleTimeCounter < 0)
+        {
+            bc.enabled = true;
+            isWorking = true;
+        }
+        else
+        {
+            isWorking = false;
+            bc.enabled = false;
+        }
+
+        FanController();
+        anim.SetBool("isWorking", isWorking);
+
+    }
+
+    private void FanController()
+    {
+        if (Mathf.Abs(idleTimeCounter) > activeTime)
+            idleTimeCounter = idleTime;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+            playerRb.velocity = new Vector2(playerRb.velocity.x, pushUpwardForce);
+        }
+    }
+
+
+}
