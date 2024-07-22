bb27a99fc8b9c1c3ab9ae781c92c9586a073eeed
ata kaleli <atakaleli123@gmail.com>
2024-07-22 14:12:02 +0300

    FallingPlatform is added

---
+++ Assets/Scripts/Platforms/FallingPlatform.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+ 
+    private Animator anim;
+    private bool isWorking = true;
+    [SerializeField] private float fallTime;
+    [SerializeField] private float fallSpeed;
+    [SerializeField] private float fallDistance;
+    private float fallTimeCounter;
+    private bool isFalling;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    
+    void Update()
+    {
+        fallTimeCounter -= Time.deltaTime;
+        
+        if(isFalling)
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x,-fallDistance,transform.position.z), fallSpeed * Time.deltaTime);
+
+
+        anim.SetBool("isWorking", isWorking);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Player>() != null)
+        {
+            isWorking = false;
+            StartCoroutine(FallPlatform());
+            
+        }
+    }
+
+    IEnumerator FallPlatform()
+    {
+        yield return new WaitForSeconds(fallTime);
+        isFalling = true;
+        Destroy(gameObject, 5f);
+    }
+
+}
