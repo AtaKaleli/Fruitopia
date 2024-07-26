b22ad1e2becae2c437ea70454602a9fd741d4491
ata kaleli <atakaleli123@gmail.com>
2024-07-26 10:53:47 +0300

    Enemy_Slime is added

---
+++ Assets/Scripts/Enemies/Slime.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : Enemy
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        WalkAround();
+        CollisionChecks();
+        AnimationController();
+
+
+
+    }
+
+    private void AnimationController()
+    {
+        anim.SetFloat("xVelocity", rb.velocity.x);
+    }
+}
