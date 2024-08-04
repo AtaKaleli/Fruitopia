cc965a6090da7dffa9eab395fff40551f7c31503
ata kaleli <atakaleli123@gmail.com>
2024-08-04 13:07:58 +0300

    EndPoint is added

---
+++ Assets/Scripts/CheckPoints/EndPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            anim.SetTrigger("reached");
+        }
+    }
+
+
+}
