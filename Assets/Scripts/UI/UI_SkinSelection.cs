ce9dc5a732c63d1a5369f29825b44d534020f200
ata kaleli <atakaleli123@gmail.com>
2024-08-09 10:23:24 +0300

    Purchase Skin system is added

--- Assets/Scripts/UI/UI_SkinSelection.cs
+++ Assets/Scripts/UI/UI_SkinSelection.cs
@@ -1,46 +1,135 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
+
+[System.Serializable]
+public struct Skin
+{
+    public string skinName;
+    public int skinPrice;
+    public bool unlocked;
+
+}
 
 public class UI_SkinSelection : MonoBehaviour
 {
-    [SerializeField] private int currentIndex;
+    [SerializeField] private int skinIndex;
     [SerializeField] private Animator skinDisplay;
     [SerializeField] private int maxIndex;
 
+    [SerializeField] private Skin[] skinList;
+    [SerializeField] private TextMeshProUGUI bankAmount;
+    [SerializeField] private TextMeshProUGUI priceText;
+    [SerializeField] private TextMeshProUGUI buySelectText;
 
-    public  void NextSkin()
+
+    private void Start()
     {
-        currentIndex++;
-        if (currentIndex > maxIndex)
-            currentIndex = 0;
+        
+        UpdateSkinDisplay();
+        LoadSkinUnlocks();
+    }
+
+    public void NextSkin()
+    {
+        skinIndex++;
+        if (skinIndex > maxIndex)
+            skinIndex = 0;
 
         UpdateSkinDisplay();
     }
 
     public void PreviousSkin()
     {
-        currentIndex--;
-        if (currentIndex < 0)
-            currentIndex = maxIndex;
+        skinIndex--;
+        if (skinIndex < 0)
+            skinIndex = maxIndex;
 
         UpdateSkinDisplay();
     }
 
     private void UpdateSkinDisplay()
     {
+
+        bankAmount.text = FruitsInBank().ToString();
+
         for (int i = 0; i < skinDisplay.layerCount; i++)
         {
             skinDisplay.SetLayerWeight(i, 0); // set weight to 0 for all layers
         }
 
-        skinDisplay.SetLayerWeight(currentIndex, 1); //set weight to 1 for desired skin
+        skinDisplay.SetLayerWeight(skinIndex, 1); //set weight to 1 for desired skin
+
+        if (skinList[skinIndex].unlocked)
+        {
+            priceText.transform.parent.gameObject.SetActive(false);
+            buySelectText.text = "Select";
+        }
+        else
+        {
+            priceText.transform.parent.gameObject.SetActive(true);
+            priceText.text = "Price:   " + skinList[skinIndex].skinPrice.ToString();
+            buySelectText.text = "Buy";
+
+        }
+
+    }
+
+    private void LoadSkinUnlocks()
+    {
+        for (int i = 0; i < skinList.Length; i++)
+        {
+            string skinName = skinList[i].skinName;
+            bool skinUnlocked = PlayerPrefs.GetInt(skinName + "Unlocked", 0) == 1;
+
+            print(skinName);
+            print(skinUnlocked);
+
+            if (skinUnlocked || i== 0)
+                skinList[i].unlocked = true;
+
+        }
+    }
+
+    private void BuySkin(int index)
+    {
+        
+        if (HaveEnoguhFruits(skinList[index].skinPrice) == false)
+        {
+            print("not enough fruits");
+            return;
+        }
+
+        
+        string skinName = skinList[index].skinName;
+        skinList[index].unlocked = true;
+
+        PlayerPrefs.SetInt(skinName + "Unlocked", 1);
     }
 
     public void SelectSkin()
     {
-        SkinManager.instance.SetSkinId(currentIndex);
+        if (skinList[skinIndex].unlocked == false)
+            BuySkin(skinIndex);
+        else
+            SkinManager.instance.SetSkinId(skinIndex);
+
+        UpdateSkinDisplay();
     }
 
+    public int FruitsInBank()
+    {
+        return PlayerPrefs.GetInt("TotalFruitsAmount");
+    }
 
+    private bool HaveEnoguhFruits(int price)
+    {
+        if(FruitsInBank() > price)
+        {
+            PlayerPrefs.SetInt("TotalFruitsAmount", FruitsInBank() - price);
+            return true;
+        }
+
+        return false;
+    }
 }
